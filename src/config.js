1ec7c5687d3bd0abc4babc73780d960f1e9e50a1
Robert Lejeune <rlejeune@viridisterra.com>
2021-06-25 15:24:49 -0400

    fixed the config

--- src/config.js
+++ src/config.js
@@ -4,17 +4,17 @@ const config = {
     'pk_test_51J5WVhGsXKyvGxq8AAaCVWnHjsQqaBZSuXfaLBzJjqcbu6vcUe1IKCbF4ZwHN0fpg3Xrm5N8UJbfnoZJKNiaAjgX007J06IJJa',
   s3: {
     REGION: 'us-east-1',
-    BUCKET: 'notes-app-upload-bob',
+    BUCKET: 'dev-notes-infra-s3-uploads4f6eb0fd-18oawivki78fz',
   },
   apiGateway: {
     REGION: 'us-east-1',
-    URL: 'https://knzs5vrn96.execute-api.us-east-1.amazonaws.com/prod',
+    URL: 'https://n6ue4dr047.execute-api.us-east-1.amazonaws.com/dev',
   },
   cognito: {
     REGION: 'us-east-1',
-    USER_POOL_ID: 'us-east-1_mSQuoZ6Gm',
-    APP_CLIENT_ID: '4fpa2cjnube5tqsqf7o08oqvgv',
-    IDENTITY_POOL_ID: 'us-east-1:7c294804-3018-4dd0-a2fb-dc672c76c8d9',
+    USER_POOL_ID: 'us-east-1_tl9k0X25c',
+    APP_CLIENT_ID: '1eh1r0368a2sa693ivqrn90d56',
+    IDENTITY_POOL_ID: 'us-east-1:80b06cd9-7df3-4d71-89bb-00d55bb7c376',
   },
 };
 
